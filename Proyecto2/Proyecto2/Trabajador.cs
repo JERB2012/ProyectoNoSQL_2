0d1619a4971b2d4ec30221df0d9d0be8e055ab7f
Josef Ramirez Berrocal <jerb1999.2012@gmail.com>
2020-08-07 11:38:42 -0600

    Menu inicial de la aplicacion

---
+++ Proyecto2/Proyecto2/Trabajador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2
+{
+    public class Trabajador
+    {
+        public void Trabajo01()
+        {
+            var laOpcion = string.Empty;
+            while (laOpcion != "X")
+            {
+                DesplegarMenu();
+                laOpcion = LeaLaOpcion();
+            }
+            
+
+        }
+
+        private void DesplegarMenu()
+        {
+            Console.WriteLine("Menu Principal del Proyecto 2:");
+            Console.WriteLine("1.Listar Clientes.");
+            Console.WriteLine("2.Listar Inventario.");
+            Console.WriteLine("3.Listar Ordenes.");
+            Console.WriteLine("X.Salir");
+        }
+
+        private string LeaLaOpcion()
+        {
+            string elResultado = Console.ReadLine();
+            return elResultado;
+        }
+    }
+}
